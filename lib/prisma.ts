da73086b880307b68588c113049035d61295a7ac
HagisawaYuki <hagisawa7110@gmail.com>
2026-05-17 16:59:19 +0900

    3回目コミット

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
-}
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient
+// }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient()
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma
+// }
+export const dynamic = 'force-dynamic';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+
+export default prisma;
